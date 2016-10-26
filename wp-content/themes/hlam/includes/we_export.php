d58d3b6faf5da15b46c2141c03f7e7aa3539801c
engine77 <zaur.ka.00@mail.ru>
2016-10-26 14:15:30 +0300

    25

--- wp-content/themes/hlam/includes/we_export.php
+++ wp-content/themes/hlam/includes/we_export.php
@@ -5,31 +5,31 @@
 				<h3 class="zk_ex_caption">Мы вывозим</h3>
 				<div class="zk_export_one">
 					<div class="zk_img_ex_box">
-						<img src="img/zk_export.png" alt="">
+						<img src="img/vyv-1.png" alt="">
 				    </div>
 					<div class="zk_first_child">Старую мебель вещи и книги</div>
 				</div>
 				<div class="zk_export_one">
 					<div class="zk_img_ex_box"> 
-						<img src="img/zk_export2.png" alt="">
+						<img src="img/vyv-2.png" alt="">
 					</div>
 					<div>Строительный мусор, ванны и радиаторы</div>
 				</div>
 				<div class="zk_export_one">
 					<div class="zk_img_ex_box">
-					 	<img src="img/zk_export3.png" alt="">
+					 	<img src="img/vyv-3.png" alt="">
 					 </div>
 					<div>Пианино</div>
 				</div>
 				<div class="zk_export_one">
 					<div class="zk_img_ex_box">
-						<img src="img/zk_export4.png" alt="">
+						<img src="img/vyv-4.png" alt="">
 					</div>
 					<div>Старые окна</div>
 				</div>
 				<div class="zk_export_one">
 					<div class="zk_img_ex_box">
-						<img src="img/zk_export5.png" alt="">
+						<img src="img/vyv-5.png" alt="">
 					</div>
 					<div>Бытовую технику</div>
 				</div>
