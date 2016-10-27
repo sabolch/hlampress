ae75774908ffa8c2b273fde3d070ec8b88871e28
hatuhov <hatuhov@yandex.ru>
2016-10-27 14:10:07 +0300

    q

--- wp-content/themes/hlam/page.php
+++ wp-content/themes/hlam/page.php
@@ -757,7 +757,7 @@
 <section class="ah-comptext">
     <div class="container">
 	    <div class="row">
-		   <div class="col-md-12 col-sm-12 col-xs-12">
+		   <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12">
 				<?php the_field('txt81'); ?>
 			</div>
 		</div>
